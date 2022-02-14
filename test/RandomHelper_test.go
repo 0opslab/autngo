4372989c4f66006de5497e15dfa8c710a1c807ea
0opslab <909070781@qq.com>
2022-02-14 21:53:35 +0800

    go

---
+++ test/RandomHelper_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+
+	Autngo "github.com/0opslab/autngo"
+	"github.com/0opslab/autngo/random"
+)
+
+func TestRandom_GoRandomString(t *testing.T) {
+	//系统自动的随机数正确使用方法
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < 10; i++ {
+		vcode := fmt.Sprintf("%06v", rnd.Int31n(1000000))
+		fmt.Println(vcode)
+	}
+}
+
+func TestRandom_String(t *testing.T){
+	//封装的随机数方法使用
+	rand := Autngo.RandomHelper.NewRandom()
+	for i := 0; i < 10; i++ {
+		//注意此处是有序的 而不是随机的
+		fmt.Println(rand.GetUUID())
+		fmt.Println(rand.GetUint32())
+
+		fmt.Println(Autngo.RandomHelper.String(8,random.Numeric))
+		fmt.Println(Autngo.RandomHelper.String(32,random.Alphanumeric))
+		fmt.Println(Autngo.RandomHelper.RandomString(10))
+		fmt.Println(Autngo.RandomHelper.RandomInt(10))
+	}
+}
