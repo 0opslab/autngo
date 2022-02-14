866966709e469df4f13ab48b5e0d7c0eb1b36435
0opslab <909070781@qq.com>
2022-02-14 22:01:53 +0800

    go

--- sys/SysHepler.go
+++ sys/SysHepler.go
@@ -6,9 +6,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"strconv"
 	"strings"
-	"syscall"
 )
 
 //封装与操作系统相关的一些操作
@@ -32,30 +30,31 @@ func (ss *SysHelper) OsInfo() {
 
 // windows获取系统盘符
 func (ss *SysHelper) GetLogicalDrives() []string {
-	if runtime.GOOS == "windows" {
-		// windows系统
-		kernel32 := syscall.MustLoadDLL("kernel32.dll")
-		GetLogicalDrives := kernel32.MustFindProc("GetLogicalDrives")
-		n, _, _ := GetLogicalDrives.Call()
-		s := strconv.FormatInt(int64(n), 2)
-		var drives_all = []string{"A:", "B:", "C:", "D:", "E:", "F:", "G:", "H:", "I:",
-			"J:", "K:", "L:", "M:", "N:", "O:", "P：", "Q：", "R：", "S：", "T：", "U：",
-			"V：", "W：", "X：", "Y：", "Z："}
-		temp := drives_all[0:len(s)]
-		var d []string
-		for i, v := range s {
-			if v == 49 {
-				l := len(s) - i - 1
-				d = append(d, temp[l])
-			}
-		}
-		var drives []string
-		for i, v := range d {
-			drives = append(drives[i:], append([]string{v}, drives[:i]...)...)
-		}
-		return drives
-	}
+	// if runtime.GOOS == "windows" {
+	// 	// windows系统
+	// 	kernel32 := syscall.MustLoadDLL("kernel32.dll")
+	// 	GetLogicalDrives := kernel32.MustFindProc("GetLogicalDrives")
+	// 	n, _, _ := GetLogicalDrives.Call()
+	// 	s := strconv.FormatInt(int64(n), 2)
+	// 	var drives_all = []string{"A:", "B:", "C:", "D:", "E:", "F:", "G:", "H:", "I:",
+	// 		"J:", "K:", "L:", "M:", "N:", "O:", "P：", "Q：", "R：", "S：", "T：", "U：",
+	// 		"V：", "W：", "X：", "Y：", "Z："}
+	// 	temp := drives_all[0:len(s)]
+	// 	var d []string
+	// 	for i, v := range s {
+	// 		if v == 49 {
+	// 			l := len(s) - i - 1
+	// 			d = append(d, temp[l])
+	// 		}
+	// 	}
+	// 	var drives []string
+	// 	for i, v := range d {
+	// 		drives = append(drives[i:], append([]string{v}, drives[:i]...)...)
+	// 	}
+	// 	return drives
+	// }
 	return nil
+
 }
 
 /**
